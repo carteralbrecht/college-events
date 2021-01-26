625b95c75031c238e96ab6540932497bb98c6722
Carter Albrecht <cartera@knights.ucf.edu>
2021-01-26 16:14:10 -0500

    setting up

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const path = require('path');
+
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+module.exports = router;
